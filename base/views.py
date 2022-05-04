a238ec299ca321468b0a4da1fd7de17b5f9126ab
A B M Asadullah <abmasadullah@gmail.com>
2022-05-04 12:51:48 +0600

    Add template and show models

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
-def tasklist(request):
-    return HttpResponse("To Do")
+
+class TaskList(ListView):
+    model = Task
+
